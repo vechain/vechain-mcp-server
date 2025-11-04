5d84c23af2f164680f1f4478f70814c4b204b373
DavNej <david.nejar@gmail.com>
2025-11-04 12:03:28 +0100

    add decode-event tool

--- src/tools/index.ts
+++ src/tools/index.ts
@@ -5,6 +5,6 @@ export {
   searchDocsVechainKit,
   searchDocsVevote,
 } from './search-docs'
-
+export { decodeEvent } from './thor/decode-event'
 export { getBlock } from './thor/get-block'
 export { getTransaction } from './thor/get-transaction'
