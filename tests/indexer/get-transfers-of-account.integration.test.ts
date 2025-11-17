60eb638fd1ba5f51d80e23ae8ae539acd63a2112
freemanzMrojo <miguel.rojo@vechain.org>
2025-11-17 18:02:52 +0000

    added test by token address

--- tests/indexer/get-transfers-of-account.integration.test.ts
+++ tests/indexer/get-transfers-of-account.integration.test.ts
@@ -32,6 +32,22 @@ describe('Indexer Get Transfers Of Account', () => {
     expect((response.structuredContent as { data: any[] }).data.length).toBeGreaterThan(10)
   })
 
+  test('should get transfers for a valid tokenAddress (B3TR)', async () => {
+    const B3TR_TOKEN_ADDRESS = '0x5ef79995FE8a89e0812330E4378eB2660ceDe699'
+    const response = await client.callTool({
+      name: 'getTransfersOfAccount',
+      arguments: {
+        tokenAddress: B3TR_TOKEN_ADDRESS,
+      },
+    })
+    expect(response.content).toBeDefined()
+    expect(response.structuredContent).toBeDefined()
+    expect((response.structuredContent as { network: string }).network).toBeDefined()
+    expect((response.structuredContent as { ok: boolean }).ok).toBe(true)
+    expect((response.structuredContent as { data: any[] }).data).toBeDefined()
+    expect((response.structuredContent as { data: any[] }).data.length).toBeGreaterThan(0)
+  })
+
   test('should fail when neither address nor tokenAddress is provided', async () => {
     const response = await client.callTool({
       name: 'getTransfersOfAccount',
